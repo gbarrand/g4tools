45331286ec32d3331f7ed85c207b48037f78a7fc
Guy Barrand <guy.barrand@gmail.com>
2021-01-22 15:03:54 +0100

    update

---
+++ test/cpp/polyhedron_Windows.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file tools.license for terms.
+
+
+#define TOOLS_SCREEN_MGR Windows
+
+#include <tools/Windows/sg_viewer>
+
+#include "polyhedron_common.icc"
