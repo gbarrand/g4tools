082a281122c25a1747b5e9ac689935e5498a2c1e
Guy Barrand <guy.barrand@gmail.com>
2021-01-22 11:02:27 +0100

    update

---
+++ test/cpp/polyhedron_X11.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file tools.license for terms.
+
+#define TOOLS_SCREEN_MGR X11
+
+#include <tools/X11/sg_viewer>
+
+#include "polyhedron_common.icc"
---
+++ test/cpp/polyhedron_metal.mm
@@ -0,0 +1,10 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file tools.license for terms.
+
+//exlib_build__cppflags -DTOOLS_METAL_DRAW_WHEN_NEEDED
+
+#define TOOLS_SCREEN_MGR metal
+
+#include <tools/metal/sg_viewer.hm>
+
+#include "polyhedron_common.icc"
